e90233c9af3e4dfb1a391fd6f46858692b445a3b
Nicolas Lopes <162803649+BobSkywalker@users.noreply.github.com>
2024-11-12 17:05:02 -0300

    Inicio

---
+++ app.js
@@ -0,0 +1,17 @@
+const options = {
+    threshold: 0.2,
+}
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else{
+            entry.target.classList.remove('show')
+        }
+    })
+}, options)
+
+
+const card = document.querySelectorAll('.jogo')
+card.forEach((el) => observer.observe(el))
